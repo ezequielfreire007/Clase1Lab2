ddcd61c0d370e74dd7a4f3cd4a6c30a587b4e9f8
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-03-16 10:27:58 -0300

    Saludo al mundo

--- Laboratorio2Ejercitacion1/Laboratorio2Ejercitacion1/Program.cs
+++ Laboratorio2Ejercitacion1/Laboratorio2Ejercitacion1/Program.cs
@@ -9,6 +9,8 @@ namespace Laboratorio2Ejercitacion1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine();
         }
     }
 }
